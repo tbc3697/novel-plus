e84f294c7158a1bfa8bb9acfe959dca08424c156
tbc <tbc521@163.com>
2025-07-22 17:53:44 +0800

    sync

--- novel-common/src/main/java/com/java2nb/novel/core/utils/HttpUtil.java
+++ novel-common/src/main/java/com/java2nb/novel/core/utils/HttpUtil.java
@@ -5,8 +5,8 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Administrator
@@ -16,7 +16,7 @@ public class HttpUtil {
 
     private static final String DEFAULT_CHARSET = "utf-8";
 
-    private static final Map<String, RestTemplate> REST_TEMPLATE_MAP = new HashMap<>();
+    private static final Map<String, RestTemplate> REST_TEMPLATE_MAP = new ConcurrentHashMap<>();
 
     public static String getByHttpClientWithChrome(String url, String charset) {
         log.debug("Get url：{}", url);
@@ -24,13 +24,15 @@ public class HttpUtil {
             log.error("字符编码{}无效！", charset);
             return null;
         }
-        RestTemplate restTemplate = REST_TEMPLATE_MAP.computeIfAbsent(charset, k -> RestTemplates.newInstance(charset));
+        RestTemplate restTemplate = REST_TEMPLATE_MAP.computeIfAbsent(charset,
+            k -> RestTemplates.newInstance(charset));
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.add("user-agent",
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36");
             HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
-            ResponseEntity<String> forEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+            ResponseEntity<String> forEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity,
+                String.class);
             log.debug("Response code：{}", forEntity.getStatusCode());
             if (forEntity.getStatusCode() == HttpStatus.OK) {
                 return forEntity.getBody();
