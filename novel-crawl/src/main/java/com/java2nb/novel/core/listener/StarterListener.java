bf530aa478800afb53f4f86d4990abaa8a277302
tbc <tbc521@163.com>
2025-11-03 19:13:52 +0800

    thread name

--- novel-crawl/src/main/java/com/java2nb/novel/core/listener/StarterListener.java
+++ novel-crawl/src/main/java/com/java2nb/novel/core/listener/StarterListener.java
@@ -91,9 +91,7 @@ public class StarterListener implements ServletContextInitializer {
                     }
 
                 }
-            }).start();
-
-
+            }, "crawl-update").start();
         }
 
         new Thread(() -> {
@@ -129,6 +127,6 @@ public class StarterListener implements ServletContextInitializer {
                 }
 
             }
-        }).start();
+        }, "crawl-single").start();
     }
 }
