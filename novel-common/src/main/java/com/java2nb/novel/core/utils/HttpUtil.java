8084c80e54c965c4787ab1627c3751e3c2802bee
tbc <tbc521@163.com>
2025-11-14 19:22:24 +0800

    code

--- novel-common/src/main/java/com/java2nb/novel/core/utils/HttpUtil.java
+++ novel-common/src/main/java/com/java2nb/novel/core/utils/HttpUtil.java
@@ -34,7 +34,7 @@ public class HttpUtil {
             if (cookie != null && !cookie.isEmpty()) {
                 headers.add("cookie", cookie);
             }
-            headers.add("user-agent",
+            headers.add("User-Agent",
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36");
             HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
             ResponseEntity<String> forEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity,
