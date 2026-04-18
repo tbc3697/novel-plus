1efa7f80542f79635625ba76431ef07bf7cbf9d9
liling <tbc521@163.com>
2026-04-18 13:30:45 +0800

    idworker

--- novel-front/src/main/java/com/java2nb/novel/core/filter/LoggingFilter.java
+++ novel-front/src/main/java/com/java2nb/novel/core/filter/LoggingFilter.java
@@ -1,51 +1,61 @@
 package com.java2nb.novel.core.filter;
 
-
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.function.BiConsumer;
 
+
 @Slf4j
-public class LoggingFilter implements Filter {
+public class LoggingFilter extends OncePerRequestFilter {
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String uri = request.getRequestURI();
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
 
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-        // 获取请求基本信息
-        String uri = httpRequest.getRequestURI();
-        if (uri.contains("/mcp")) {
-            chain.doFilter(request, response);
-            return;
-        }
-        String method = httpRequest.getMethod();
-        String queryString = httpRequest.getQueryString();
-
-
-        // 记录开始时间
         long startTime = System.currentTimeMillis();
 
         try {
-            // 执行后续过滤器和目标资源
-            chain.doFilter(request, response);
+            filterChain.doFilter(requestWrapper, responseWrapper);
         } finally {
+            String reqBody = "";
+            String respBody = "";
+            if (uri.startsWith("/mcp")) {
+                reqBody = getBody(requestWrapper.getContentAsByteArray());
+                respBody = getBody(responseWrapper.getContentAsByteArray());
+            }
+
             // 计算并打印耗时
             long endTime = System.currentTimeMillis();
             long duration = endTime - startTime;
+            var method = request.getMethod();
 
             BiConsumer<String, String[]> logAction = duration > 100 ? log::error : log::info;
             // 实时打印请求耗时日志
             logAction.accept(
-                    "[REQUEST_TIME] {} {} | Status: {} | Params: {} | Duration: {} ms",
-                    new String[]{method, uri, String.valueOf(httpResponse.getStatus()), queryString, String.valueOf(duration)}
+                    "[REQUEST_TIME] {} {} | Status: {}| Duration: {} ms | Params: {} | respBody: {}",
+                    new String[]{method, uri, String.valueOf(response.getStatus()), reqBody, String.valueOf(duration), respBody}
             );
+            responseWrapper.copyBodyToResponse();
         }
     }
 
+    private String getBody(byte[] buf) {
+        if (buf == null || buf.length == 0) return "";
+        if (buf.length > 10240) return "[too large]";
+        return new String(buf, StandardCharsets.UTF_8);
+    }
 }
